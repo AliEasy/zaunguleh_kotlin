7e2e6634336c5037489b4b5bb08e896f4893e034
a.izadyar <A.Izadyar@citydi.com>
2025-02-03 15:37:31 +0330

    feat: no item to show message when reminder list is empty

--- app/src/main/java/top/easyware/zanguleh/features/daily_counter/presentation/DailyCounterScreen.kt
+++ app/src/main/java/top/easyware/zanguleh/features/daily_counter/presentation/DailyCounterScreen.kt
@@ -6,12 +6,15 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
@@ -27,13 +30,16 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import top.easyware.zanguleh.R
 import top.easyware.zanguleh.core.screens.Screens
+import top.easyware.zanguleh.core.util.UiText
 import top.easyware.zanguleh.features.daily_counter.presentation.components.FilterSection
 import top.easyware.zanguleh.features.daily_counter.presentation.components.ReminderItem
 
@@ -123,25 +129,42 @@ fun DailyCounterScreen(
                     )
                 }
                 Spacer(modifier = Modifier.height(16.dp))
-                LazyColumn(
-                    modifier = Modifier
-                        .padding(horizontal = 25.dp)
-                        .fillMaxSize()
-                        .padding(it),
-                ) {
-                    items(state.reminders) { reminder ->
-                        ReminderItem(
-                            modifier = Modifier.fillMaxWidth(),
-                            reminder = reminder,
-                            onTap = {
-                                navController.navigate(
-                                    Screens.SubmitReminderScreen.withOptionalArgs(
-                                        mapOf(
-                                            "reminderId" to reminder.reminderId.toString()
+                if (state.reminders.isNotEmpty()) {
+                    LazyColumn(
+                        modifier = Modifier
+                            .padding(horizontal = 25.dp)
+                            .fillMaxSize()
+                            .padding(it),
+                    ) {
+                        items(state.reminders) { reminder ->
+                            ReminderItem(
+                                modifier = Modifier.fillMaxWidth(),
+                                reminder = reminder,
+                                onTap = {
+                                    navController.navigate(
+                                        Screens.SubmitReminderScreen.withOptionalArgs(
+                                            mapOf(
+                                                "reminderId" to reminder.reminderId.toString()
+                                            )
                                         )
                                     )
-                                )
-                            },
+                                },
+                            )
+                        }
+                    }
+                } else {
+                    Box(
+                        contentAlignment = Alignment.Center,
+                        modifier = Modifier.fillMaxSize(),
+                    ) {
+                        Text(
+                            UiText.StringResource(R.string.nothing_found_to_show).asString(),
+                            style = MaterialTheme.typography.titleMedium,
+                            modifier = Modifier
+                                .width(200.dp)
+                                .height(100.dp)
+                                .wrapContentHeight(),
+                            textAlign = TextAlign.Center
                         )
                     }
                 }
