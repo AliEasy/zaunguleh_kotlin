73c381503d13327594618a0ed242faffff3e4dc2
a.izadyar <A.Izadyar@citydi.com>
2025-02-03 15:21:41 +0330

    chore: remove filter from counter page

--- app/src/main/java/top/easyware/zanguleh/features/daily_counter/presentation/DailyCounterScreen.kt
+++ app/src/main/java/top/easyware/zanguleh/features/daily_counter/presentation/DailyCounterScreen.kt
@@ -21,7 +21,6 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -29,9 +28,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
@@ -88,18 +85,18 @@ fun DailyCounterScreen(
                             style = MaterialTheme.typography.headlineLarge,
                         )
                     },
-                    navigationIcon = {
-                        IconButton(
-                            onClick = {
-                                viewModel.onEvent(DailyCounterEvent.ToggleFilterSection)
-                            }) {
-                            Icon(
-                                imageVector = ImageVector.vectorResource(R.drawable.filter_outline),
-                                contentDescription = context.getString(R.string.filter),
-                                tint = MaterialTheme.colorScheme.onSecondary
-                            )
-                        }
-                    },
+//                    navigationIcon = {
+//                        IconButton(
+//                            onClick = {
+//                                viewModel.onEvent(DailyCounterEvent.ToggleFilterSection)
+//                            }) {
+//                            Icon(
+//                                imageVector = ImageVector.vectorResource(R.drawable.filter_outline),
+//                                contentDescription = context.getString(R.string.filter),
+//                                tint = MaterialTheme.colorScheme.onSecondary
+//                            )
+//                        }
+//                    },
                 )
             },
 
